feb8c9f63310599d4f25a46329455ab2afba9141
murraycopps <murraycopps@gmail.com>
2025-11-11 10:46:38 -0500

    added more pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,9 +12,16 @@
 	<a class=" flex items-center px-4" href="/">
 		<CircleDanceIcon size={12} bold />
 	</a>
+	<Divider />
+	<a class="flex items-center px-4" href="/"> Home </a>
+	<Divider />
+
+	<a class="flex items-center px-4" href="/about"> About </a>
+	<Divider />
+	<a class="flex items-center px-4" href="/contact"> Contact </a>
 
 	<div class="flex-1"></div>
-	<h2 class="mr-4 text-2xl ">Murray Copps Portfolio</h2>
+	<h2 class="mr-4 text-2xl">Murray Copps Portfolio</h2>
 </header>
 
 <main class="large overflow-y-scroll lg:mt-16 lg:pt-4">
@@ -43,5 +50,4 @@
 		background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 2000 20' preserveAspectRatio='none'%3E%3Cpath d='M0 0h2000v12.5c-50 0-50-6.25-100-6.25s-50 6.25-100 6.25-50-6.25-100-6.25-50 6.25-100 6.25-50-6.25-100-6.25-50 6.25-100 6.25-50-6.25-100-6.25-50 6.25-100 6.25-50-6.25-100-6.25-50 6.25-100 6.25-50-6.25-100-6.25-50 6.25-100 6.25-50-6.25-100-6.25-50 6.25-100 6.25-50-6.25-100-6.25-50 6.25-100 6.25-50-6.25-100-6.25-50 6.25-100 6.25-50-6.25-100-6.25S50 12.5 0 12.5V0z' fill='%23A50021'/%3E%3C/svg%3E");
 		background-repeat: repeat-x;
 	}
-	
 </style>
--- src/routes/PagePreview.svelte
+++ src/routes/PagePreview.svelte
@@ -1,27 +1,35 @@
 <script lang="ts">
-	let { name, description, image, url, githubUrl, longdescription = "" } = $props();
+	let { name, description, image, url, githubUrl, longdescription = '' } = $props();
 	const images: any = import.meta.glob(
 		['$lib/images/**.jpg', '$lib/images/**.png', '$lib/images/**.svg'],
 		{ eager: true, as: 'url' }
 	);
-	let expanded = false;
+	let expanded = $state(false);
 </script>
 
-<div class="flex w-full flex-col items-center justify-center text-white">
-	<div class="flex w-full max-w-2xl flex-col items-center justify-center gap-4 rounded-2xl p-4">
+<div
+	class="bg-oglala-800 flex w-full flex-col items-center justify-center rounded-2xl p-4 text-white"
+>
+	<div class="flex w-full max-w-2xl flex-col items-center justify-center gap-4 rounded-2xl">
 		<h1 class="text-center text-3xl font-bold">{name}</h1>
-		<p class="text-center text-lg max-h-60 overflow-auto">{description}</p>
+		<p class="max-h-60 overflow-auto text-center text-lg">{description}</p>
 		{#if longdescription}
 			<button
-				class="mb-2 rounded bg-oglala-500 px-4 py-2 hover:bg-oglala-600"
+				class="bg-oglala-500 hover:bg-oglala-600 mb-2 rounded px-4 py-2"
 				on:click={() => (expanded = !expanded)}
 			>
 				{expanded ? 'Show Less' : 'Read More'}
 			</button>
 		{/if}
-		{#if longdescription && expanded}
-			<p class="text-center text-lg max-h-60 overflow-auto">{longdescription}</p>
-		{/if}
+		<div class="relative w-full">
+			{#if longdescription && expanded}
+				<div class="absolute top-0 right-0 left-0 z-10 bg-oglala-700 rounded-lg p-4">
+					<p class="max-h-60 overflow-auto text-center text-lg">
+						{longdescription}
+					</p>
+				</div>
+			{/if}
+		</div>
 		{#if image}
 			<img src={images['/src/lib/images/' + image]} alt={name} class=" w-full rounded-lg" />
 		{/if}
@@ -36,5 +44,3 @@
 		</div>
 	</div>
 </div>
-
-
