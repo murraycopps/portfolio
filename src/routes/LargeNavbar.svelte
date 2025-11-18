09059a90562eedaad8bc0b53e54ab361728fd3cc
murraycopps <murraycopps@gmail.com>
2025-11-18 10:38:10 -0500

    made navbar mobile responsive

---
+++ src/routes/LargeNavbar.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import CircleDanceIcon from '$lib/images/CircleDanceIcon.svelte';
+	import Divider from './Divider.svelte';
+    </script>
+<header
+	class="bg-oglala-500 fixed top-0 right-0 left-0 z-50 flex-row items-center gap-2 p-2 align-middle text-xl text-white hidden lg:flex"
+>
+	<h2 class="ml-4 text-3xl relative name mb-4">
+		Murray Copps
+		<span class="absolute top-6 left-30 ">
+			Portfolio
+		</span>
+	</h2>
+	<div class="flex-1"></div>
+	<a class="flex items-center px-4" href="/"> Work </a>
+	<Divider />
+	<a class="flex items-center px-4" href="/about"> About </a>
+	<Divider />
+	<a class="flex items-center px-4" href="/contact"> Contact </a>
+	<Divider />
+	<a class=" flex items-center px-4" href="/">
+		<CircleDanceIcon size={12} bold />
+	</a>
+</header>
+
+<style>
+	.name {
+		font-family: 'Grechen Fuemen';
+	}
+	* {
+		transition: all 0.5s ease-in-out;
+	}
+	header {
+		font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+		z-index: 1000;
+	}
+
+	header::after {
+		content: '';
+		position: absolute;
+		bottom: -30px;
+		left: 0;
+		width: 300%;
+		height: 30px;
+		background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 2000 20' preserveAspectRatio='none'%3E%3Cpath d='M0 0h2000v12.5c-50 0-50-6.25-100-6.25s-50 6.25-100 6.25-50-6.25-100-6.25-50 6.25-100 6.25-50-6.25-100-6.25-50 6.25-100 6.25-50-6.25-100-6.25-50 6.25-100 6.25-50-6.25-100-6.25-50 6.25-100 6.25-50-6.25-100-6.25-50 6.25-100 6.25-50-6.25-100-6.25-50 6.25-100 6.25-50-6.25-100-6.25-50 6.25-100 6.25-50-6.25-100-6.25-50 6.25-100 6.25-50-6.25-100-6.25S50 12.5 0 12.5V0z' fill='%23A50021'/%3E%3C/svg%3E");
+		background-repeat: repeat-x;
+	}
+</style>
---
+++ src/routes/SmallNavbar.svelte
@@ -0,0 +1,84 @@
+<script>
+	import CircleDance from '$lib/images/CircleDance.png';
+	import Divider from './Divider.svelte';
+	import { clickOutside } from '$lib/utils/clickOutside.js';
+
+	let out = false;
+</script>
+
+<header
+	class="fixed top-0 left-0 z-50 flex h-screen align-middle text-white lg:hidden"
+	use:clickOutside
+	onoutsideclick={() => (out = false)}
+>
+	<button
+		class=" fixed top-0 left-0 z-50 flex flex-col items-center p-4"
+		onclick={() => (out = !out)}
+	>
+		<img src={CircleDance} alt="Circle Dance" class="h-12 w-12" />
+		<div class="fixed top-18 flex h-3 w-12 flex-col">
+			<span
+				class="absolute h-1 w-full rounded-full bg-white {out
+					? 'top-rotate top-1'
+					: 'top-0  rotate-0'}"
+			></span>
+			<span
+				class="absolute h-1 w-full rounded-full bg-white {out
+					? 'bottom-rotate bottom-1'
+					: 'bottom-0 rotate-0'}"
+			>
+			</span>
+		</div>
+	</button>
+	<div
+		class="bg-oglala-500 fixed top-0 right-1/3 left-0 flex h-full flex-col items-center gap-2 p-2 pb-4 pt-20 align-middle text-xl {out
+			? 'circle-clip-visible'
+			: 'circle-clip-hidden'}"
+	>
+		<a
+			class="hover:bg-oglala-600 flex items-center rounded-sm px-8 py-2"
+			href="/"
+			onclick={() => (out = false)}>My Work</a
+		>
+		<Divider horizontal />
+        <a
+            class="hover:bg-oglala-600 flex items-center rounded-sm px-8 py-2"
+            href="/about"
+            onclick={() => (out = false)}>About</a
+        >
+        <Divider horizontal />
+        <a
+            class="hover:bg-oglala-600 flex items-center rounded-sm px-8 py-2"
+            href="/contact"
+            onclick={() => (out = false)}>Contact</a
+        >
+        <div class="flex-1"></div>
+		<h2 class=" text-2xl name">Murray Copps Portfolio</h2>
+        
+	</div>
+</header>
+
+<style>
+	.name {
+		font-family: 'Grechen Fuemen';
+	}
+	.circle-clip-hidden {
+		clip-path: circle(0% at 0 0);
+	}
+
+	.circle-clip-visible {
+		clip-path: circle(150% at 0 0);
+	}
+	* {
+		transition: all 0.5s ease-in-out;
+	}
+	.top-rotate {
+		transform: rotate(30deg);
+	}
+	.bottom-rotate {
+		transform: rotate(-30deg);
+	}
+	.top-18 {
+		top: 4.5rem;
+	}
+</style>
