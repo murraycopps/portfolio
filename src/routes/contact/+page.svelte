234291ae2111f7a007a52a4a21db4f1aef78229a
murraycopps <murraycopps@gmail.com>
2025-11-18 11:48:09 -0500

    maade contact page mobile responsive

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -2,21 +2,29 @@
 	import nationals from '$lib/images/nationals.JPEG';
 </script>
 
-<div class="grid grid-cols-2 gap-16 p-16 text-white h-full">
-    <div class="flex flex-col items-center justify-center">
-	<img src={nationals} alt="Murray Copps at Nationals" class="rounded-4xl max-w-128" />
-
-    </div>
+<div class="grid h-full gap-16 p-4 lg:p-16 text-white lg:grid-cols-2">
+	<div class="flex flex-col items-center justify-center w-full max-w-screen">
+		<img src={nationals} alt="Murray Copps at Nationals" class="w-full max-w-128 rounded-4xl" />
+	</div>
 	<div class="flex flex-col items-center justify-start gap-4 pt-20">
-        <h1 class="w-full text-8xl font-bold">Contact Me</h1>
-        <p class="text-3xl">
-            If you'd like to get in touch with me, you can email me at
-            <a href="mailto:murraycopps@gmail.com" class="underline text-oglala-400 hover:text-oglala-300">murraycopps@gmail.com</a>
-        </p>
-        <p class="text-3xl">
-            Check out my GitHub and LinkedIn profiles for more information about my work and experience:
-            <a href="https://github.com/murraycopps" class="underline text-oglala-400 hover:text-oglala-300">GitHub</a> and
-            <a href="https://www.linkedin.com/in/murray-copps-a0aa3525a" class="underline text-oglala-400 hover:text-oglala-300">LinkedIn</a>.
-        </p>
-    </div>
+		<h1 class="w-full text-5xl md:text-6xl lg:text-8xl font-bold">Contact Me</h1>
+		<p class="text-2xl lg:text-3xl">
+			If you'd like to get in touch with me, you can email me at
+			<a href="mailto:murraycopps@gmail.com" class="text-oglala-400 hover:text-oglala-300 underline"
+				>murraycopps@gmail.com</a
+			>
+		</p>
+		<p class="text-2xl lg:text-3xl">
+			Check out my GitHub and LinkedIn profiles for more information about my work and experience:
+			<a
+				href="https://github.com/murraycopps"
+				class="text-oglala-400 hover:text-oglala-300 underline">GitHub</a
+			>
+			and
+			<a
+				href="https://www.linkedin.com/in/murray-copps-a0aa3525a"
+				class="text-oglala-400 hover:text-oglala-300 underline">LinkedIn</a
+			>.
+		</p>
+	</div>
 </div>
